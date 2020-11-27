45ac60f11d9bc447c81307dcfc3dd85959469534
Tenundor <71014004+Tenundor@users.noreply.github.com>
2020-11-27 20:58:56 +0300

    Перенёс чуствительные данные в переменные окружения

--- project/settings.py
+++ project/settings.py
@@ -1,22 +1,24 @@
 from dotenv import load_dotenv
 import os
 
+
+load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'HOST': os.getenv("HOST"),
+        'PORT': os.getenv("PORT"),
+        'NAME': os.getenv("NAME"),
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD"),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ROOT_URLCONF = "project.urls"
 
