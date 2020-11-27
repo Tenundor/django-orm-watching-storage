9dd01fea48394599b411e986477cb4e7638df877
Tenundor <71014004+Tenundor@users.noreply.github.com>
2020-11-27 21:20:03 +0300

    Вынес настройку DEBUG в переменные окружения

--- project/settings.py
+++ project/settings.py
@@ -1,24 +1,25 @@
-from dotenv import load_dotenv
+from environs import Env
 import os
 
 
-load_dotenv()
+env = Env()
+env.read_env()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': os.getenv("HOST"),
-        'PORT': os.getenv("PORT"),
-        'NAME': os.getenv("NAME"),
-        'USER': os.getenv("USER"),
-        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': env("HOST"),
+        'PORT': env("PORT"),
+        'NAME': env("NAME"),
+        'USER': env("USER"),
+        'PASSWORD': env("PASSWORD"),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
-DEBUG = os.getenv("DEBUG")
+DEBUG = env.bool("DEBUG")
 
 ROOT_URLCONF = "project.urls"
 
